63bf5a3be7f63496b720200838f7b0714a2d0400
Nicky van Urk <nickyvanurk@gmx.com>
2023-03-03 23:57:32 +0100

    Detach station centrifuge code from station angular velocity

--- src/app/station.ts
+++ src/app/station.ts
@@ -3,10 +3,13 @@ import { Entity } from './entity';
 
 export class Station extends Entity {
     mesh: THREE.Object3D;
+    upperCentrifugeSpeed = Math.PI / 180;
+    lowerCentrifugeSpeed = Math.PI / 180;
+    upperCentrifugeRotation = 0;
+    lowerCentrifugeRotation = 0;
 
     constructor(mesh: THREE.Object3D) {
         super(mesh);
-        this.angularVelocity.y = 0.01;
 
         this.mesh.position.copy(this.position);
         this.mesh.rotation.setFromVector3(this.rotation);
@@ -14,13 +17,16 @@ export class Station extends Entity {
 
     update(dt: number) {
         super.update(dt);
+
+        this.upperCentrifugeRotation += this.upperCentrifugeSpeed * dt;
+        this.lowerCentrifugeRotation += this.lowerCentrifugeSpeed * dt;
     }
 
     render(alpha: number, dt: number) {
         super.render(alpha, dt);
 
-        // this.mesh.children[0].rotation.z = this.rotation.y + this.angularVelocity.y * dt * alpha;
-        // this.mesh.children[1].rotation.z = -this.rotation.y - this.angularVelocity.y * dt * alpha;
-        // this.mesh.children[2].rotation.z = -this.rotation.y - this.angularVelocity.y * dt * alpha;
+        this.mesh.children[0].rotation.z = this.lowerCentrifugeRotation + this.lowerCentrifugeSpeed * dt * alpha;
+        this.mesh.children[1].rotation.z = -this.upperCentrifugeRotation - this.upperCentrifugeSpeed * dt * alpha;
+        this.mesh.children[2].rotation.z = -this.upperCentrifugeRotation - this.upperCentrifugeSpeed * dt * alpha;
     }
 }
