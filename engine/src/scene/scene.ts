d643b238c042eb23239e93d328cbbff6be8d08f4
Nicky van Urk <nickyvanurk@gmx.com>
2023-03-05 22:42:04 +0100

    Extract GLTFLoader

--- engine/src/scene/scene.ts
+++ engine/src/scene/scene.ts
@@ -12,15 +12,11 @@ interface IScene {
 }
 
 export class Scene implements IScene {
-    load = {
-        gltf: this.loadGLTF.bind(this),
-    };
-
-    add = {
-        mesh: this.getGLTF.bind(this),
-    };
+    load = { gltf: this.loadGLTF.bind(this) };
+    add = { mesh: this.getGLTF.bind(this) };
 
     private loadingManager = new THREE.LoadingManager();
+    private loader = { gltf: new GLTFLoader(this.loadingManager) };
     private meshes: { [key: string]: GLTF } = {};
 
     constructor(readonly name = 'default') {
@@ -48,8 +44,7 @@ export class Scene implements IScene {
     }
 
     private loadGLTF(name: string, path: string) {
-        const loader = new GLTFLoader(this.loadingManager);
-        loader.load(path, gltf => (this.meshes[name] = gltf));
+        this.loader.gltf.load(path, gltf => (this.meshes[name] = gltf));
     }
 
     private getGLTF(name: string) {
