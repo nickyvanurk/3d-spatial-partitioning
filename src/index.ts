98e04cd4a52f555fd67f623315f2cd86a12300a8
Nicky van Urk <nickyvanurk@gmx.com>
2023-03-05 21:35:49 +0100

    Add a loading scene for testing

--- src/index.ts
+++ src/index.ts
@@ -6,25 +6,43 @@ import './style.css';
 
 import * as Merlin from 'merlin';
 
+class Loading extends Merlin.Scene {
+    constructor() {
+        super('loading');
+    }
+
+    init() {
+        console.log('init ' + this.name);
+    }
+
+    preload() {
+        console.log('preload ' + this.name);
+    }
+
+    create() {
+        console.log('create ' + this.name);
+    }
+}
+
 class Main extends Merlin.Scene {
     constructor() {
         super('main');
     }
 
     init() {
-        console.log('init');
+        console.log('init ' + this.name);
     }
 
     preload() {
-        console.log('preload');
+        console.log('preload ' + this.name);
     }
 
     create() {
-        console.log('create');
+        console.log('create ' + this.name);
     }
 }
 
 new Merlin.Application({
     backgroundColor: 0x131a29,
-    scene: Main,
+    scene: [Loading, Main],
 });
