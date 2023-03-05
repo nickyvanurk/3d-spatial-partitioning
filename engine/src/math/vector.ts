35651abd246ca14cffb840b41ce41280f46aeb49
Nicky van Urk <nickyvanurk@gmx.com>
2023-03-05 15:15:26 +0100

    Add Vector3 helpers

--- engine/src/math/vector.ts
+++ engine/src/math/vector.ts
@@ -12,4 +12,48 @@ export class Vector3 {
         this.y = y;
         this.z = z;
     }
+
+    copy(v: Vector3) {
+        this.set(v.x, v.y, v.z);
+    }
+
+    add(v: Vector3) {
+        return new Vector3(this.x + v.x, this.y + v.y, this.z + v.z);
+    }
+
+    addScalar(s: number) {
+        return new Vector3(this.x + s, this.y + s, this.z + s);
+    }
+
+    addInPlace(v: Vector3) {
+        this.x += v.x;
+        this.y += v.y;
+        this.z += v.z;
+    }
+
+    addScalarInPlace(s: number) {
+        this.x += s;
+        this.y += s;
+        this.z += s;
+    }
+
+    multiply(v: Vector3) {
+        return new Vector3(this.x * v.x, this.y * v.y, this.z * v.z);
+    }
+
+    multiplyScalar(s: number) {
+        return new Vector3(this.x * s, this.y * s, this.z * s);
+    }
+
+    multiplyInPlace(v: Vector3) {
+        this.x *= v.x;
+        this.y *= v.y;
+        this.z *= v.z;
+    }
+
+    multiplyScalarInPlace(s: number) {
+        this.x *= s;
+        this.y *= s;
+        this.z *= s;
+    }
 }
