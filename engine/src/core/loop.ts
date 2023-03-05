ba210e1883338237f7b03713486a6e8cb36515a1
Nicky van Urk <nickyvanurk@gmx.com>
2023-03-05 19:13:42 +0100

    Expose Time class

--- engine/src/core/loop.ts
+++ engine/src/core/loop.ts
@@ -1,13 +1,4 @@
-export class Time {
-    static now = 0;
-    static last = 0;
-    static deltaTime = 0;
-    static fixedDeltaTime = 0;
-    static time = 0;
-    static fixedTime = 0;
-    static accumulator = 0;
-    static alpha = 0;
-}
+import { Time } from './time';
 
 export class Loop {
     private fixedCallback: () => void;
---
+++ engine/src/core/time.ts
@@ -0,0 +1,10 @@
+export class Time {
+    static now = 0;
+    static last = 0;
+    static deltaTime = 0;
+    static fixedDeltaTime = 0;
+    static time = 0;
+    static fixedTime = 0;
+    static accumulator = 0;
+    static alpha = 0;
+}
