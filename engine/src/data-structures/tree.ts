b08b9cb2a0445f7353061668a5ed70e86bbe7c43
Nicky van Urk <nickyvanurk@gmx.com>
2023-03-07 17:28:12 +0100

    Add generic tree data structure; for managing scenes

---
+++ engine/src/data-structures/tree.ts
@@ -0,0 +1,68 @@
+export interface ITreeNode<T> {
+    data: T;
+    parent: ITreeNode<T>;
+    children: Array<ITreeNode<T>>;
+}
+
+export interface ITree<T> {
+    root: ITreeNode<T>;
+
+    add(data: T, parent: ITreeNode<T>): ITreeNode<T>;
+    find(data: T, parent: ITreeNode<T>): ITreeNode<T> | undefined;
+    traverse(cb: (data: T) => void, parent: ITreeNode<T>): void;
+    delete(data: T, parent: ITreeNode<T>): ITreeNode<T>;
+}
+
+export class TreeNode<T> implements ITreeNode<T> {
+    data: T;
+    parent: ITreeNode<T>;
+    children: Array<ITreeNode<T>>;
+
+    constructor(data?: T, parent?: ITreeNode<T>) {
+        this.data = data || undefined;
+        this.parent = parent || undefined;
+        this.children = [];
+    }
+}
+
+export class Tree<T> implements ITree<T> {
+    root: ITreeNode<T>;
+
+    constructor(data: T) {
+        this.root = new TreeNode(data);
+    }
+
+    add(data: T, parent = this.root) {
+        const node = new TreeNode(data, parent);
+        parent.children.push(node);
+        return node;
+    }
+
+    find(data: T, parent = this.root): TreeNode<T> | undefined {
+        if (parent.data === data) {
+            return parent;
+        }
+
+        for (const child of parent.children) {
+            const node = this.find(data, child);
+            if (node) return node;
+        }
+
+        return undefined;
+    }
+
+    traverse(cb: (data: T) => void, parent = this.root) {
+        cb(parent.data);
+        for (const child of parent.children) {
+            this.traverse(cb, child);
+        }
+    }
+
+    delete(data: T, parent = this.root) {
+        const node = this.find(data, parent);
+        if (node) {
+            node.parent.children.splice(node.parent.children.indexOf(node), 1);
+        }
+        return node;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,79 @@
 import './style.css';
+import { Tree } from '../engine/src/data-structures/tree';
+
 // import { App } from './app/app';
 
 // const app = new App();
 // app.run();
 
-import * as mln from 'merlin';
-import { Main } from './scenes';
+// import * as mln from 'merlin';
+// import { Main } from './scenes';
 
-new mln.Application({
-    clearColor: 0x131a29,
-    scene: Main,
+// new mln.Application({
+//     clearColor: 0x131a29,
+//     scene: Main,
+// });
+
+class Scene {
+    constructor(readonly name?: string) {}
+}
+
+const scenes = new Tree(new Scene('root'));
+
+const secondNode = scenes.add(new Scene('second'));
+scenes.add(new Scene('2.1'), secondNode);
+scenes.add(new Scene('2.2'), secondNode);
+scenes.add(new Scene('2.3'), secondNode);
+
+const thirdNode = scenes.add(new Scene('third'));
+scenes.add(new Scene('3.1'), thirdNode);
+scenes.add(new Scene('3.2'), thirdNode);
+scenes.add(new Scene('3.3'), thirdNode);
+
+const fourhtNode = scenes.add(new Scene('fourth'));
+scenes.add(new Scene('4.1'), fourhtNode);
+scenes.add(new Scene('4.2'), fourhtNode);
+scenes.add(new Scene('4.3'), fourhtNode);
+
+scenes.traverse((scene: Scene) => {
+    console.log(scene);
 });
+
+const found = scenes.find(thirdNode.data);
+console.log(found.data);
+
+console.log('deleting ', scenes.delete(found.data).data);
+
+scenes.traverse((scene: Scene) => {
+    console.log(scene);
+});
+
+// class Scenes {
+//     value = new Tree(new Scene());
+//     map: { [key: string]: TreeNode<Scene> } = {};
+
+//     constructor() {
+//         super(new Scene());
+//         this.map['root'] = this.root;
+//     }
+
+//     add(name: string, parent = '') {
+//         if (!parent) {
+//             this.map[name] = super.add(new Scene(name));
+//         } else {
+//             this.map[name] = super.add(new Scene(name), this.map[parent]);
+//         }
+//     }
+
+//     // create(name: string) {
+//     //     const scene = new Scene(name);
+//     //     //
+//     // }
+
+//     // find(name: string): Scene {
+//     //     return new Scene(name);
+//     //     //
+//     // }
+// }
+
+// class Scene implements FSM
