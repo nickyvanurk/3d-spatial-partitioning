69846d0fe6d24c94e0d6feba870edc030c846abe
Nicky van Urk <nickyvanurk@gmx.com>
2023-03-08 12:46:50 +0100

    Add update functions

--- src/index.ts
+++ src/index.ts
@@ -39,15 +39,44 @@ export class TreeNode {
             child.traverseChildren(cb);
         }
     }
+
+    fixedUpdate() {
+        // do nothing
+    }
+
+    update() {
+        // do nothing
+    }
 }
 
 class SceneTree {
-    root: TreeNode = new TreeNode();
-    currentScene: TreeNode = null;
+    public root: TreeNode = new TreeNode();
+
+    private currentScene: TreeNode = null;
 
     setCurrentScene(scene: TreeNode) {
         if (scene && scene.getParent() !== this.root) throw new Error('Not a child node of root');
         this.currentScene = scene;
+    }
+
+    fixedUpdate(node = this.currentScene) {
+        if (node) {
+            node.fixedUpdate();
+        }
+
+        for (const child of node.children) {
+            this.fixedUpdate(child);
+        }
+    }
+
+    update(node = this.currentScene) {
+        if (node) {
+            node.update();
+        }
+
+        for (const child of node.children) {
+            this.update(child);
+        }
     }
 }
 
