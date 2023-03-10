c58c09f7a135f896123f2566de2c7b6900994f68
Nicky van Urk <nickyvanurk@gmx.com>
2023-03-10 17:25:18 +0100

    Disable the use of the engine right now; too complex

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,23 @@
 import './style.css';
-// import { Tree } from '../engine/src/data-structures/tree2';
+import { App } from './app/app';
 
-// import { App } from './app/app';
-
-// const app = new App();
-// app.run();
-
-import * as mln from 'merlin';
-import { Camera3D } from '../engine/src/scene/camera_3d';
-
-class Main extends mln.Scene {
-    init() {
-        this.name = 'main';
-
-        const camera = new Camera3D();
-        this.addChild(camera);
-    }
-}
-
-const app = new mln.Application({
-    clearColor: 0x131a29,
-    scene: Main,
-});
+const app = new App();
 app.run();
+
+// import * as mln from 'merlin';
+// import { Camera3D } from '../engine/src/scene/camera_3d';
+
+// class Main extends mln.Scene {
+//     init() {
+//         this.name = 'main';
+
+//         const camera = new Camera3D();
+//         this.addChild(camera);
+//     }
+// }
+
+// const app = new mln.Application({
+//     clearColor: 0x131a29,
+//     scene: Main,
+// });
+// app.run();
