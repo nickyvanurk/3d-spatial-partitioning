c4c2084da8869f0130f48029b092d1c773b469f1
Nicky van Urk <nickyvanurk@gmx.com>
2023-03-10 09:07:21 +0100

    Add Camera3D node

---
+++ engine/src/scene/camera_3d.ts
@@ -0,0 +1,21 @@
+import * as THREE from 'three';
+import { Node } from './node';
+
+export class Camera3D extends Node {
+    fov = 71;
+    near = 0.05;
+    far = 4000;
+    current = false;
+
+    private camera: THREE.Camera;
+
+    constructor() {
+        super();
+
+        //@TODO: Refactor aspect into:
+        // Size2 viewport_size = get_viewport().get_camera_rest_size()
+        // viewport_size.aspect()
+        const aspect = window.innerWidth / window.innerHeight;
+        this.camera = new THREE.PerspectiveCamera(this.fov, aspect, this.near, this.far);
+    }
+}
