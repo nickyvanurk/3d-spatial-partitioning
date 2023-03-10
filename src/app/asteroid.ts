e751f3ae7f9990f8d8a170e4b507a3e275778cc2
Nicky van Urk <nickyvanurk@gmx.com>
2023-03-10 17:35:10 +0100

    Shrink the asteroid sphere mesh radius

--- src/app/asteroid.ts
+++ src/app/asteroid.ts
@@ -5,7 +5,7 @@ import { Context } from './types';
 
 export class Asteroid extends Entity {
     constructor(ctx: Context, position = new Vector3(), rotation = new Vector3()) {
-        const geometry = new THREE.SphereGeometry(15, 32, 16);
+        const geometry = new THREE.SphereGeometry(5, 32, 16);
         const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
         const sphere = new THREE.Mesh(geometry, material);
         super(new Mesh(sphere), position, rotation);
