f5f14ba283d7b9b05d8d7cbb7aaa46ef054a9482
Nicky van Urk <nickyvanurk@gmx.com>
2023-03-12 21:42:53 +0100

    Add a resource to asteroids

--- src/app/asteroid.ts
+++ src/app/asteroid.ts
@@ -2,18 +2,29 @@ import * as THREE from 'three';
 import { Mesh, Vector3 } from 'merlin';
 import { Entity } from './entity';
 import { Context } from './types';
+import { type MeshBasicMaterial } from 'three';
 
 export class Asteroid extends Entity {
+    resource = 100;
+
+    private material: MeshBasicMaterial;
+
     constructor(ctx: Context, position = new Vector3(), rotation = new Vector3()) {
         const geometry = new THREE.SphereGeometry(5, 32, 16);
         const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
         const sphere = new THREE.Mesh(geometry, material);
         super(new Mesh(sphere), position, rotation);
         ctx.scene.add(this.mesh.value);
+        this.material = material;
     }
 
     update(dt: number) {
         super.update(dt);
+
+        if (this.resource <= 0) {
+            this.resource = 0;
+            this.material.color.set(0xff0000);
+        }
     }
 
     render(alpha: number, dt: number) {
