349e95ccd816b862e1cc48c02f1bb5e8a55ce023
Nicky van Urk <nickyvanurk@gmx.com>
2023-03-13 20:25:37 +0100

    Remove commented engine code

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,3 @@ import { App } from './app/app';
 
 const app = new App();
 app.run();
-
-// import * as mln from 'merlin';
-// import { Camera3D } from '../engine/src/scene/camera_3d';
-
-// class Main extends mln.Scene {
-//     init() {
-//         this.name = 'main';
-
-//         const camera = new Camera3D();
-//         this.addChild(camera);
-//     }
-// }
-
-// const app = new mln.Application({
-//     clearColor: 0x131a29,
-//     scene: Main,
-// });
-// app.run();
